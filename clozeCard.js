318a3569c3c4d350c92d1566723c01f336ef486e
yogashri1126 <ypradhan.pge@texas.edu>
2017-09-01 04:07:54 -0500

    I do not know why I'm getting empy values when I'm passing the question and answer

--- clozeCard.js
+++ clozeCard.js
@@ -1,32 +1,60 @@
-module.exports = clozeCard
-// var partial=" "
-// var fullText= " "
+//module.exports = clozeCard
+    // var partial=" "
+var question = ""
+var result = ""
+var answers = []
 
-var clozeCard= new ClozeCard()
+//var clozeCard = new ClozeCard()
 
-function ClozeCard(fullText, cloze){
 
-	this.fullText= fullText
-	this.cloze= cloze;
 
-this.partial=function(){
-return	this.fullText.replace(cloze, "___________ ")}
+if (process.argv[2] === "fullText") {
+    for (i = 3; i < process.argv.length; i++) {
+        question = question+ process.argv[i] + " "
+    }
+	console.log(question)
+}
+
+if (process.argv[2] === "result") {
+    for (i = 3; i < process.argv.length; i++) {
+        result = result+ process.argv[i] + " "
+    }
+console.log(result)
+}
+
+ var firstPresidentCloze = new ClozeCard(
+    question, result);
+ 	console.log(question)
+ 	console.log(result)
+
+	function ClozeCard(fullText, cloze) {
+
+    this.fullText = fullText
+    this.cloze = cloze;
+
+    this.partial = function() {
+        return this.fullText.replace(cloze, "___________ ")
+    }
+}
+//var clozeCard = new ClozeCard()
+// "George Washington"
+if (process.argv[2] === "cloze") {
+    //console.log(firstPresidentCloze.cloze);
+    answers.push(firstPresidentCloze.cloze)
+    //console.log(answers)
+    console.log(firstPresidentCloze.partial());
+    console.log(firstPresidentCloze.fullText)
 
 }
 
 
 
-var firstPresidentCloze = new ClozeCard(
-	"George Washington was the first president of the United States.", "George Washington");
-
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
 
 // " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial()); 
+
 
 // "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText)
+
 
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-var brokenCloze = new ClozeCard("This doesn't work", "oops");
+//var brokenCloze = new ClozeCard("This doesn't work", "oops");
